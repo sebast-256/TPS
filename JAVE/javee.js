e0fc4bffb71ce502328e9b727f26e604e864d165
MarceloAlanoca <elfalopade2024@gmail.com>
2026-03-30 16:58:13 -0300

    jola

---
+++ JAVE/javee.js
@@ -0,0 +1,8 @@
+function meses(){
+
+    const losmeses = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+        "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+    console.log(losmeses);
+}
