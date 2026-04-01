3cb9471c0d8604b7ad0b30bde3a49a631f1fdf1b
Alumno <alumno@et26.local>
2026-04-01 14:27:43 -0300

    ejercicio 2

--- JAVE/javes/jave 2.js
+++ JAVE/javes/jave 2.js
@@ -1,7 +1,26 @@
-function maynor(){
+function mates(){
 
     var valores = [
-        True, 5, False, "Hola", "Adiós", 2
+        true, 5, false, "hola", "adiós", 2
     ];
-    console.log(num);
+
+    var suma = (valores[1]+valores[5]);
+    var resta = (valores[1]-valores[5]);
+    var multiplicacion = (valores[1]*valores[5]);
+    var división = (valores[1]/valores[5]);
+    var potencia = (valores[1]**valores[5]);
+
+    console.log("Suma: ", suma);
+    console.log("Resta: ", resta);
+    console.log("Multiplicación: ", multiplicacion);
+    console.log("División: ", división);
+    console.log("Potencia: ", potencia);
+
+    if(valores[3].length > valores[4].length){
+        console.log("La afirmación es: ", valores[2]);
+    }
+    else if(valores[3].length < valores[4].length){
+        console.log("La afirmación es: ", valores[0]);
+    }
+
 }
